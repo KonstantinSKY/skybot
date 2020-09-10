81046884958a19f95173a6bcf81d251a168c1e36
Konstantin Smolnikov <sky012877@gmail.com>
2020-09-09 23:43:53 -0700

    Add datatime method

--- oanda.py
+++ oanda.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import security
+from datetime import datetime
 
 
 class Oanda:
+    addr = "https://api-fxpractice.oanda.com/v3/"
 
     def __init__(self, auth):
-        self.addr = "https://api-fxpractice.oanda.com/v3/"
         self.headers = {'Content-Type': 'application/json',
                         'Authorization': 'Bearer ' + auth}
         self.accounts = self.get_accounts()['accounts']
@@ -15,7 +16,7 @@ class Oanda:
         print(self.accounts_ids)
 
     def rest_get(self, sub_str):
-        res = requests.get(f'{self.addr}{sub_str}', headers=self.headers)
+        res = requests.get(f'{Oanda.addr}{sub_str}', headers=self.headers)
         return json.loads(res.text)
 
     def get_accounts(self):
@@ -24,6 +25,24 @@ class Oanda:
     def get_account_details(self, acc_id):
         r = self.rest_get(f"accounts/{acc_id}")
         print(r)
+
+    @classmethod
+    def get_time_obj(cls, date_time):
+        date_time, microsec = date_time.rstrip('Z').split('.')
+        date_time_obj = datetime.strptime(date_time, "%Y-%m-%dT%H:%M:%S")
+        date_time_obj = date_time_obj.replace(microsecond=int(microsec))
+        # print(date_time_obj)
+        return date_time_obj
+
+    @classmethod
+    def get_date_time(cls, dt_obj):
+        month = str(dt_obj.month).zfill(2)
+        day = str(dt_obj.day).zfill(2)
+        hour = str(dt_obj.hour).zfill(2)
+        minute = str(dt_obj.minute).zfill(2)
+        second = str(dt_obj.second).zfill(2)
+        microsecond = str(dt_obj.microsecond).zfill(6)
+        return f'{dt_obj.year}-{month}-{day}T{hour}:{minute}:{second}.{microsecond}Z'
 
 
 class Account(Oanda):
@@ -63,23 +82,25 @@ class Instrument(Oanda):
         super().__init__(auth)
         self.name = name
         self.granularity = granularity
+        self.price = f'price=M&granularity={self.granularity}'
         self.cache = {}
 
     def get_last_candles_by_count(self, count):
         if count > 5000:
             count = 5000
-        r = self.rest_get(f"instruments/{self.name}/candles?count={count}&price=M&granularity={self.granularity}")['candles']
+        r = self.rest_get(f"instruments/{self.name}/candles?count={count}&{self.price}")['candles']
         self.cache = r
-        print(r)
-        print(len(r))
         return r
 
-    def get_last_candles_by_time(self, granularity):
-        pass
-
+    def get_candles_by_time(self, from_time, to_time=None):
+        if to_time is None:
+            r = self.rest_get(f"instruments/{self.name}/candles?count=5000&from={from_time}&{self.price}")['candles']
+        else:
+            r = self.rest_get(f"instruments/{self.name}/candles?from={from_time}&to={to_time}&{self.price}")['candles']
+        self.cache = r                                                                                                        
+        return r
 
 acc = Account(security.auth_key, security.account1_id)
-
 # acc.get_details()
 acc.get_summary()
 # acc.get_instruments()
@@ -88,8 +109,22 @@ print(acc.instruments)
 print(len(acc.instruments))
 
 instr = Instrument(security.auth_key, 'EUR_USD', 'S5')
-instr.get_last_candles_by_count(5001)
+#instr.get_last_candles_by_count(5001)
+time = datetime.now()
 
+# print(time)
+# print(time.date())
+# print(time.time())
+# print(f'{time.date()}T{time.time()}Z')
+# print(datetime.timestamp(time))
+# print(datetime.timestamp(time)+5)
+# dt_obj1 = Oanda.get_time_obj('2020-09-09T22:00:48.932952Z')
+# print(datetime.timestamp(dt_obj1))
+# print(Oanda.get_date_time(dt_obj1))
+#candles = instr.get_candles_by_time('2020-09-09T20:20:48.932952Z', Oanda.get_date_time(datetime.now()))
+candles = instr.get_candles_by_time('2020-09-09T0:20:48.932952Z')
+print(candles)
+print(len(candles))
 
 #oanda.get_accounts()
 # print(oanda.accounts)
