1c6868b66dd22f33239fdff745eb426dd105cf0a
Konstantin Smolnikov <sky012877@gmail.com>
2020-09-30 22:48:43 -0700

    help for class CandleReceiver

--- candle_receiver.py
+++ candle_receiver.py
@@ -7,13 +7,19 @@ class CandleReceiver:
     """  Asynchronous API receiver for getting candle data from instruments"""
 
     def __init__(self, instruments=None):
+        """
+        API receiver object constructor
+        :param instruments: list of str -> list of instruments names
+        :var self.instruments: list of str -> list of instruments names
+        :var self.instr_objects: list of objects -> list objects of instrument for receiving candle data
+        """
         if instruments is None:
             self.instruments = Account(oanda_auth_keys[1], oanda_auth_keys[1]['id']).get_instruments_names()
         else:
             self.instruments = instruments
         self.instr_objects = []
+
         for instrument in self.instruments:
-            print('for:', instrument)
             self.instr_objects.append(Instrument(oanda_auth_keys[1], instrument, 'S5'))
 
 
