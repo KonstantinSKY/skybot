85a5c424a8361705031c96eba789bd2c22dff8c6
Konstantin Smolnikov <sky012877@gmail.com>
2020-10-02 10:16:09 -0700

    added help for def init

--- rest_api.py
+++ rest_api.py
@@ -3,25 +3,34 @@ from requests.exceptions import HTTPError
 from logger import Logger
 import json
 from time import sleep, time
-import os
 from security import auth_key
 
-print(os.getcwd())
 log_rest = Logger(__name__)
 
 
 class RestAPI:
+    """ RestAPI class for connection with any remote API """
 
     def __init__(self, headers=None):
-        self.client = requests.Session()
-        self.client.headers = headers if headers else {}
+        """
+        RestAPI object constructor
+        :param headers: dict object -> request headers parameters
+        :var self.session: object -> session for non async connection
+        :var self.headers: -> request headers parameters
+        :var self.timeout: -> request timeout in sec
+        :var self.attempts: -> connection attempts count, in connection error case
+        :var self.attempts_delay: -> delay between connection attempts
+        :var self.duration: -> duration of request in sec
+        """
+        self.session = requests.Session()
+        self.headers = headers if headers else {}
         self.timeout = 0
         self.attempts = 10
         self.attempt_delay = 5
         self.duration = 0
 
     def __del__(self):
-        self.client.close()
+        self.session.close()
 
     def get(self, url, timeout=30):
         self.timeout = timeout
@@ -29,7 +38,7 @@ class RestAPI:
         for attempt in range(1, self.attempts + 1):
             try:
                 start_time = time()
-                response = self.client.get(url, timeout=self.timeout)
+                response = self.session.get(url, timeout=self.timeout, headers=self.headers)
                 self.duration = time() - start_time
                 response.raise_for_status()
                 # if
@@ -46,7 +55,7 @@ class RestAPI:
 
         for attempt in range(1, self.attempts + 1):
             try:
-                response = self.client.get(url, timeout=self.timeout)
+                response = self.session.get(url, timeout=self.timeout)
                 response.raise_for_status()
                 return response
 
@@ -59,8 +68,8 @@ class RestAPI:
 
 if __name__ == "__main__":
     rest = RestAPI()
-    print('rest.client', rest.client)
-    print('rest.client.headers', rest.client.headers)
+    print('rest.client', rest.session)
+    print('rest.client.headers', rest.session.headers)
     print(time())
     print('rest_time', rest.duration)
     auth = f'Bearer {auth_key}'
@@ -71,8 +80,8 @@ if __name__ == "__main__":
     print('rest_time', rest.duration)
 
     rest2 = RestAPI(h)
-    print('rest2.client', rest2.client)
-    print('rest2.client.headers', rest2.client.headers)
+    print('rest2.client', rest2.session)
+    print('rest2.client.headers', rest2.session.headers)
 
     print(rest2.get(
         "https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5"))
@@ -85,8 +94,8 @@ if __name__ == "__main__":
          }
 
     rest3 = RestAPI(h)
-    print('rest2.client', rest3.client)
-    print('rest2.client.headers', rest3.client.headers)
+    print('rest2.client', rest3.session)
+    print('rest2.client.headers', rest3.session.headers)
     print(rest3.get(
         "https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5"))
     print('rest_time', rest3.duration)
