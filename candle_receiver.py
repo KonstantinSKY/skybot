04cf6dc9c936e289d30c143f07ab57d28fa76f47
Konstantin Smolnikov <sky012877@gmail.com>
2020-10-07 23:30:45 -0700

    candle_receiver.py new version step 3

--- candle_receiver.py
+++ candle_receiver.py
@@ -37,26 +37,47 @@ class CandleReceiver(OandaAPI):
         """ Get all last candles"""
         print('instr_obj.url_candles', instr_obj.url_candles)
         self.params = instr_obj.params
+        # await self.__waiter__()
+        print('now:', datetime.now().timestamp())
+        print('int:', self.params['from'])
+        if datetime.now().timestamp() - int(float(self.params['from'])) < 10:
+            print('canceled ==========================================>')
+            print('now:', datetime.now().timestamp())
+            print('int:', self.params['from'])
+            return
+
         res = await self.get_async(instr_obj.url_candles)
         instr_obj.candle_cache = json.loads(res)['candles']
         print('instr_obj.url_candles', instr_obj.url_candles)
         print(len(instr_obj.candle_cache))
-        print(instr_obj.candle_cache[-1])
+        # print(instr_obj.candle_cache[-1])
         instr_obj.verify_candles()
         if instr_obj.candle_cache:
             instr_obj.set_candles()
-        #instr_obj.waiter()
+
+    async def __waiter__(self):
+        log.prn_log_info(f'Waiter: Next start time: {self.params["from"]})')
+        log.prn_log_info(f'Now:  {datetime.now().timestamp()} {datetime.now()} ')
+
+        if (datetime.now().timestamp() - int(float(self.params['from']))) < 60:
+            check_time = datetime.now().timestamp() // 5 * 5 + 5
+            log.prn_log_info(f'NEED TO sleep: {check_time - datetime.now().timestamp()}')
+            await asyncio.sleep(check_time - datetime.now().timestamp())
 
 
 if __name__ == "__main__":
     """for checking"""
     cr = CandleReceiver(oanda_auth_keys[1])
-    #print(cr.instruments)
-   # print(cr.instr_objects)
-    tasks = [cr.loop.create_task(cr.get_last_candles(instr_obj)) for instr_obj in cr.instr_objects]
-    start_time = datetime.now().timestamp()
-    cr.loop.run_until_complete(asyncio.wait(tasks))
-    print("time for :", datetime.now().timestamp() - start_time)
+    # print(cr.instruments)
+    # print(cr.instr_objects)
+
+
+    while True:
+        tasks = [cr.loop.create_task(cr.get_last_candles(instr_obj)) for instr_obj in cr.instr_objects]
+        start_time = datetime.now().timestamp()
+        cr.loop.run_until_complete(asyncio.wait(tasks))
+        print("time for :", datetime.now().timestamp() - start_time)
+        # sleep(10)
 
     #
     # print('headers', cr.instr_objects[0].headers)
