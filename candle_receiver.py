7427cbc757827b64666e0d8c36ea9459e828f6f2
Konstantin Smolnikov <sky012877@gmail.com>
2020-10-08 23:51:47 -0700

    refactoring and adding help

--- candle_receiver.py
+++ candle_receiver.py
@@ -2,7 +2,6 @@ from account import Account
 from instruments import Instrument
 from security import oanda_auth_keys
 import asyncio
-from time import sleep
 from oanda_api import OandaAPI
 import json
 from datetime import datetime
@@ -29,6 +28,7 @@ class CandleReceiver(OandaAPI):
         :param instruments: list of str -> list of instruments names
         :var self.instruments: list of str -> list of instruments names
         :var self.instr_objects: list of objects -> objects of instrument list for receiving candle data
+        :var self.pause: int, seconds -> pause between requests for one instrument
         """
         super().__init__(auth)
         if instruments is None:
@@ -39,36 +39,28 @@ class CandleReceiver(OandaAPI):
 
         for instrument in self.instruments:
             self.instr_objects.append(Instrument(auth, instrument, 'S5'))
-
+        self.pause = 5
         self.loop = asyncio.get_event_loop()
 
     async def get_last_candles(self, instr_obj):
-        """ Async Getting  all last candles """
+        """ Async Getting  all last candles
+        :param instr_obj: object -> instrument object
+        :var self.params: dict -> parameters for url request
+        """
         print('instr_obj.url_candles', instr_obj.url_candles)
         self.params = instr_obj.params
-        if datetime.now().timestamp() - int(float(self.params['from'])) < 5:
-            log.prn_log_info(f'Canceled. Pause less then 5 sec. {instr_obj.name}, '
-                             f'from_time:, {self.params["from"]}, now: {datetime.now().timestamp()}')
+        if datetime.now().timestamp() - int(float(self.params['from'])) < self.pause:
+            # log.prn_log_info(f'Canceled. Pause less then 5 sec. {instr_obj.name}, '
+            #                  f'from_time:, {self.params["from"]}, now: {datetime.now().timestamp()}')
             return
 
         res = await self.get_async(instr_obj.url_candles, timeout=2)
         instr_obj.candle_cache = json.loads(res)['candles'] if res is not None else None
 
-        print('instr_obj.url_candles', instr_obj.url_candles)
-        print(len(instr_obj.candle_cache))
-        # print(instr_obj.candle_cache[-1])
         instr_obj.verify_candles()
+
         if instr_obj.candle_cache:
             instr_obj.set_candles()
-
-    async def __waiter__(self):
-        log.prn_log_info(f'Waiter: Next start time: {self.params["from"]})')
-        log.prn_log_info(f'Now:  {datetime.now().timestamp()} {datetime.now()} ')
-
-        if (datetime.now().timestamp() - int(float(self.params['from']))) < 60:
-            check_time = datetime.now().timestamp() // 5 * 5 + 5
-            log.prn_log_info(f'NEED TO sleep: {check_time - datetime.now().timestamp()}')
-            await asyncio.sleep(check_time - datetime.now().timestamp())
 
 
 if __name__ == "__main__":
--- rest_api.py
+++ rest_api.py
@@ -27,20 +27,24 @@ class RestAPI:
         :var self.duration: -> duration of last request in sec
         """
         self.session = requests.Session()
-        # self.aio_session = aiohttp.ClientSession()
         self.headers = headers if headers else {}
         self.params = {}
-        self.timeout = 0
+        self.timeout = 30
         self.attempts = 10
         self.attempt_delay = 5
         self.duration = 0
 
     def __del__(self):
+        """ Destructor for delete """
         self.session.close()
-        # self.aio_session.close()
 
     def get(self, url, timeout=30):
-        self.timeout = timeout
+        """
+        Get response from simple HTTP non async request
+        :param url: str : url string for request
+        :param timeout: timeout for response
+        :return: object from JSON response
+        """
 
         for attempt in range(1, self.attempts + 1):
             try:
@@ -48,8 +52,8 @@ class RestAPI:
                 response = self.session.get(url, timeout=self.timeout, headers=self.headers, params=self.params)
                 self.duration = time() - start_time
                 response.raise_for_status()
-                # if
-                return json.loads(response.text)
+                if response.text:
+                    return json.loads(response.text)
 
             except HTTPError as http_err:
                 log_rest.prn_log_err(f'HTTP error occurred: {http_err}, attempt:{attempt}, duration:{self.duration}')
@@ -79,91 +83,74 @@ class RestAPI:
         async with aiohttp.ClientSession() as session:
             try:
                 async with session.get(url, headers=self.headers, params=self.params, timeout=self.timeout) as resp:
-                    print('status:', resp.status)
-                    res = await resp.text()
-
-                return res
+                    return await resp.text()
             except Exception as Err:
                 print('+' * 100)
                 print('ERROR ', Err)
-                # print('RES ', res)
-                print('status:', resp.status)
                 print('+' * 100)
-                sleep(10)
 
-
-    def waiter(self):
-        log.prn_log_info(f'Waiter: Next start time: {self.params["from"]})')
-        log.prn_log_info(f'Now:  {datetime.now().timestamp()} {datetime.now()} ')
-
-        if (datetime.now().timestamp() - int(float(self.params['from']))) < 60:
-            check_time = datetime.now().timestamp() // 5 * 5 + 5
-            log.prn_log_info(f'NEED TO sleep: {check_time - datetime.now().timestamp()}')
-            sleep(check_time - datetime.now().timestamp())
 
 if __name__ == "__main__":
-    # rest = RestAPI()
-    # print('rest.client', rest.session)
-    # print('rest.client.headers', rest.session.headers)
-    # print(time())
-    # print('rest_time', rest.duration)
-    auth = f'Bearer {auth_key}'
+    pass
+    # # rest = RestAPI()
+    # # print('rest.client', rest.session)
+    # # print('rest.client.headers', rest.session.headers)
+    # # print(time())
+    # # print('rest_time', rest.duration)
+    # auth = f'Bearer {auth_key}'
+    # # h = {'Content-Type': 'application/json',
+    # #      'Authorization': auth,
+    # #      }
+    # #
+    # # print('rest_time', rest.duration)
+    # #
+    # # rest2 = RestAPI(h)
+    # # print('rest2.client', rest2.session)
+    # # print('rest2.client.headers', rest2.session.headers)
+    # #
+    # # print(rest2.get(
+    # #     "https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5"))
+    # #
+    # # print('rest_time2', rest2.duration)
     # h = {'Content-Type': 'application/json',
     #      'Authorization': auth,
+    #      'Accept-Datetime-Format': 'UNIX'
     #      }
+    # #
+    # # rest3 = RestAPI(h)
+    # # print('rest2.client', rest3.session)
+    # # print('rest2.client.headers', rest3.session.headers)
+    # # print(rest3.get(
+    # #     "https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5"))
+    # # print('rest_time', rest3.duration)
     #
-    # print('rest_time', rest.duration)
+    # print('get async')
+    # rest4 = RestAPI(h)
+    # loop = asyncio.get_event_loop()
+    # rest4.params = {'count': 5,
+    #                 'price': 'B',
+    #                 'granularity': 'S5'}
     #
-    # rest2 = RestAPI(h)
-    # print('rest2.client', rest2.session)
-    # print('rest2.client.headers', rest2.session.headers)
+    # instrument = ['CAD_SGD', 'GBP_NZD', 'ZAR_JPY', 'EUR_HUF', 'EUR_DKK', 'USD_MXN', 'GBP_USD', 'NZD_HKD', 'AUD_CHF', 'CAD_JPY', 'GBP_SGD', 'USD_SEK', 'AUD_HKD', 'AUD_NZD', 'AUD_JPY', 'EUR_ZAR', 'SGD_CHF', 'AUD_SGD', 'EUR_JPY', 'USD_CHF', 'USD_TRY', 'GBP_JPY', 'EUR_CZK', 'CHF_ZAR', 'EUR_TRY', 'USD_JPY', 'USD_NOK', 'TRY_JPY', 'USD_DKK', 'CHF_JPY', 'EUR_PLN', 'SGD_JPY', 'AUD_CAD', 'NZD_USD', 'EUR_CHF', 'NZD_SGD', 'USD_HKD', 'CHF_HKD', 'USD_CAD', 'USD_CNH', 'USD_CZK', 'GBP_ZAR', 'EUR_HKD', 'HKD_JPY', 'EUR_AUD', 'USD_SGD', 'EUR_SEK', 'GBP_HKD', 'EUR_NZD', 'EUR_CAD', 'USD_HUF', 'NZD_CAD', 'EUR_SGD', 'AUD_USD', 'EUR_USD', 'GBP_AUD', 'USD_PLN', 'SGD_HKD', 'CAD_HKD', 'GBP_CAD', 'USD_SAR', 'GBP_PLN', 'EUR_NOK', 'NZD_CHF', 'USD_ZAR', 'NZD_JPY', 'USD_THB', 'GBP_CHF', 'EUR_GBP', 'CAD_CHF']
+    # # instrument = ['EUR_USD']
+    # url = 'https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles'
+    # url2 = 'https://api-fxpractice.oanda.com/v3/instruments/GBP_USD/candles'
+    # url3 = 'https://api-fxpractice.oanda.com/v3/instruments/GBP_JPY/candles'
     #
-    # print(rest2.get(
-    #     "https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5"))
+    # async def prnres(url):
+    #     rest5 = RestAPI(h)
+    #     rest5.params = {'count': 5,
+    #                     'price': 'B',
+    #                     'granularity': 'S5'}
+    #     res = await rest5.get_async(url)
+    #     res = json.loads(res)['candles']
+    #     print("ressss", res)
+    #     print(len(res))
     #
-    # print('rest_time2', rest2.duration)
-    h = {'Content-Type': 'application/json',
-         'Authorization': auth,
-         'Accept-Datetime-Format': 'UNIX'
-         }
     #
-    # rest3 = RestAPI(h)
-    # print('rest2.client', rest3.session)
-    # print('rest2.client.headers', rest3.session.headers)
-    # print(rest3.get(
-    #     "https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles?count=6&price=M&granularity=S5"))
-    # print('rest_time', rest3.duration)
-
-    print('get async')
-    rest4 = RestAPI(h)
-    loop = asyncio.get_event_loop()
-    rest4.params = {'count': 5,
-                    'price': 'B',
-                    'granularity': 'S5'}
-
-    instrument = ['CAD_SGD', 'GBP_NZD', 'ZAR_JPY', 'EUR_HUF', 'EUR_DKK', 'USD_MXN', 'GBP_USD', 'NZD_HKD', 'AUD_CHF', 'CAD_JPY', 'GBP_SGD', 'USD_SEK', 'AUD_HKD', 'AUD_NZD', 'AUD_JPY', 'EUR_ZAR', 'SGD_CHF', 'AUD_SGD', 'EUR_JPY', 'USD_CHF', 'USD_TRY', 'GBP_JPY', 'EUR_CZK', 'CHF_ZAR', 'EUR_TRY', 'USD_JPY', 'USD_NOK', 'TRY_JPY', 'USD_DKK', 'CHF_JPY', 'EUR_PLN', 'SGD_JPY', 'AUD_CAD', 'NZD_USD', 'EUR_CHF', 'NZD_SGD', 'USD_HKD', 'CHF_HKD', 'USD_CAD', 'USD_CNH', 'USD_CZK', 'GBP_ZAR', 'EUR_HKD', 'HKD_JPY', 'EUR_AUD', 'USD_SGD', 'EUR_SEK', 'GBP_HKD', 'EUR_NZD', 'EUR_CAD', 'USD_HUF', 'NZD_CAD', 'EUR_SGD', 'AUD_USD', 'EUR_USD', 'GBP_AUD', 'USD_PLN', 'SGD_HKD', 'CAD_HKD', 'GBP_CAD', 'USD_SAR', 'GBP_PLN', 'EUR_NOK', 'NZD_CHF', 'USD_ZAR', 'NZD_JPY', 'USD_THB', 'GBP_CHF', 'EUR_GBP', 'CAD_CHF']
-    # instrument = ['EUR_USD']
-    url = 'https://api-fxpractice.oanda.com/v3/instruments/EUR_USD/candles'
-    url2 = 'https://api-fxpractice.oanda.com/v3/instruments/GBP_USD/candles'
-    url3 = 'https://api-fxpractice.oanda.com/v3/instruments/GBP_JPY/candles'
-
-    async def prnres(url):
-        rest5 = RestAPI(h)
-        rest5.params = {'count': 5,
-                        'price': 'B',
-                        'granularity': 'S5'}
-        res = await rest5.get_async(url)
-        res = json.loads(res)['candles']
-        print("ressss", res)
-        print(len(res))
-
-
-
-    # task = [loop.create_task(rest4.get_async(f'https://api-fxpractice.oanda.com/v3/instruments/{instr}/candles')) for instr in instrument]
-    # print(task)
-    task = [loop.create_task(prnres(f'https://api-fxpractice.oanda.com/v3/instruments/{instr}/candles')) for instr in instrument]
-
-    start_time = datetime.now().timestamp()
-    # loop.run_until_complete(asyncio.gather(rest4.get_async(url), rest4.get_async(url2), rest4.get_async(url3)))
-    loop.run_until_complete(asyncio.wait(task))
-    print('Time:', datetime.now().timestamp() - start_time)
+    # task = [loop.create_task(prnres(f'https://api-fxpractice.oanda.com/v3/instruments/{instr}/candles')) for instr in instrument]
+    #
+    # start_time = datetime.now().timestamp()
+    # # loop.run_until_complete(asyncio.gather(rest4.get_async(url), rest4.get_async(url2), rest4.get_async(url3)))
+    # loop.run_until_complete(asyncio.wait(task))
+    # print('Time:', datetime.now().timestamp() - start_time)
