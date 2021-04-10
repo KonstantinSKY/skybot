0299468c34539965ccaaee66d3197bd3b8a13bd7
sky <sky012877@gmail.com>
2021-04-10 11:06:38 -0700

    NO MESSAGE

--- sql_init.py
+++ sql_init.py
@@ -3,3 +3,10 @@
 # Author      : Stan SKY                         E-mail : sky012877@gmail.com  #
 # Description : init db                                                        #
 ################################################################################
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine =  create_engine('sqlite:///DB/test.db', echo=True)
+Base = declarative_base()
+
+
