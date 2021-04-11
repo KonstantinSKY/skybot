130189a02288edf074aa9169e293634696bc947d
sky <sky012877@gmail.com>
2021-04-10 23:06:19 -0700

    test db 2

--- sql_init.py
+++ sql_init.py
@@ -5,8 +5,19 @@
 ################################################################################
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, Float, DateTime, String
 
 engine =  create_engine('sqlite:///DB/test.db', echo=True)
 Base = declarative_base()
 
+class Quotes(Base):
+    __tablename__ = 'Quotes'
+    id = Column(Integer, primary_key=True)
+    instrument = Column(String, nullable=False )
+    bid = Column(Float, nullable=False)
+    ask = Column(Float, nullable=False) 
+    volume = Column (Integer, nullable=False)
+    time = Column(DateTime, nullable=False)
 
+Base.metadata.create_all(engine)
+print(Quotes)
